b750fecc8cf4806decf0eb42d2ac0a8baf363930
Tim Sneath <tims@microsoft.com>
2017-03-30 22:16:33 -0700

    add docking style to ToolWindow

--- VSIXProject/LearnToolWindowPackage.cs
+++ VSIXProject/LearnToolWindowPackage.cs
@@ -18,27 +18,18 @@ using Microsoft.Win32;
 
 namespace LearnVS
 {
-    /// <summary>
-    /// This is the class that implements the package exposed by this assembly.
-    /// </summary>
-    /// <remarks>
-    /// <para>
-    /// The minimum requirement for a class to be considered a valid package for Visual Studio
-    /// is to implement the IVsPackage interface and register itself with the shell.
-    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-    /// to do it: it derives from the Package class that provides the implementation of the
-    /// IVsPackage interface and uses the registration attributes defined in the framework to
-    /// register itself and its components with the shell. These attributes tell the pkgdef creation
-    /// utility what data to put into .pkgdef file.
-    /// </para>
-    /// <para>
-    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
-    /// </para>
-    /// </remarks>
+    // The minimum requirement for a class to be considered a valid package for Visual Studio
+    // is to implement the IVsPackage interface and register itself with the shell.
+    // This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    // to do it: it derives from the Package class that provides the implementation of the
+    // IVsPackage interface and uses the registration attributes defined in the framework to
+    // register itself and its components with the shell. These attributes tell the pkgdef creation
+    // utility what data to put into .pkgdef file.
+    // To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
     [ProvideMenuResource("Menus.ctmenu", 1)]
-    [ProvideToolWindow(typeof(LearnToolWindow))]
+    [ProvideToolWindow(typeof(LearnToolWindow), Style=VsDockStyle.Tabbed, Orientation=ToolWindowOrientation.Right)]
     [Guid(LearnToolWindowPackage.PackageGuidString)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
     public sealed class LearnToolWindowPackage : Package
